3fde3d98100b08b23ce6b9e96251421021c27255
BenDavis71 <69690328+BenDavis71@users.noreply.github.com>
2023-10-28 00:45:32 -0400

    Update 3_📊 _Monthly_Analysis.py

--- pages/3_📊 _Monthly_Analysis.py
+++ pages/3_📊 _Monthly_Analysis.py
@@ -61,7 +61,6 @@ g = df.groupby(['Month'], sort=False)['Expense'].sum().reset_index()
 g.columns = ['Month','Expense']
 g['Over Budget'] = g['Expense'].apply(lambda x: 'Over Budget' if x > budget else 'Under Budget')
 g = g.tail(12) #only last 12 months; possibly make this a filter in the future
-st.write(g)
 
 #graph
 fig = px.bar(
@@ -72,11 +71,11 @@ fig = px.bar(
         color_discrete_map={'Over Budget':'#ff4b4b', 'Under Budget':'#3dd56d'},
         opacity=.9
 )
-fig.add_hline(y=budget, line_width=3, line_dash="dash", line_color="#ff4b4b", opacity=.75)
-fig.update_layout(showlegend=False)
-fig.update_xaxes(showgrid=False, fixedrange=True)
-fig.update_yaxes(showgrid=False, fixedrange=True)
-fig.update_traces(opacity=.9, hovertemplate='$%{y}')
+# fig.add_hline(y=budget, line_width=3, line_dash="dash", line_color="#ff4b4b", opacity=.75)
+# fig.update_layout(showlegend=False)
+# fig.update_xaxes(showgrid=False, fixedrange=True)
+# fig.update_yaxes(showgrid=False, fixedrange=True)
+# fig.update_traces(opacity=.9, hovertemplate='$%{y}')
 st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
 
   
